83ce04ba0bb79a67e3104ee55fd39c8da6dd343c
Devin Janus <devinjanus@D.Janus-MBP-8GB>
2017-06-16 14:30:23 -0400

    fixed some basic timing and format stuff

--- src/assistant-handler.js
+++ src/assistant-handler.js
@@ -23,44 +23,64 @@ const benefitIntent = (app) => {
   console.log('--> benefit intent called')
   // app.ask('Certainly! Would you like to know about your medical, 401k, or another benefit?')
   app.askWithCarousel(app.buildRichResponse()
-    .addSimpleResponse('Certainly! If what you are looking for is not listed below simply tell me what you need')
+    .addSimpleResponse('Certainly! Here are some commonly searched employee benefits')
     .addSuggestions(
       ['Dental', 'Life', 'Tax Forms']),
-    app.buildCarousel('Most frequently searched information on Employee Benefits')
+    app.buildCarousel('Employee Benefits')
     .addItems(app.buildOptionItem(HEALTH, ['medical', 'health', 'health benefits'])
       .setTitle('Medical')
       .setDescription('Details about your health insurance policy')
       .setImage('https://samanage-assistant-demo.herokuapp.com/images/healthcare.jpg', 'health')
-    ) // add a .setImage after this works
-    .addItems(app.buildOptionItem('Retirement', ['retirement', '401k'])
-      .setTitle('Retirement')
-      .setDescription('Financial planning advice and details about your 401k')
-      .setImage('https://samanage-assistant-demo.herokuapp.com/images/retired.jpeg', 'retirement')
     )
     .addItems(app.buildOptionItem('Time Off', ['time off', 'time-off', 'pto'])
       .setTitle('Time Off')
       .setDescription('Request time off and view information on your vacation and sick leave')
       .setImage('https://samanage-assistant-demo.herokuapp.com/images/vacation.jpeg', 'time off')
     )
+    .addItems(app.buildOptionItem('Retirement', ['retirement', '401k'])
+      .setTitle('Retirement')
+      .setDescription('Financial planning advice and details about your 401k')
+      .setImage('https://samanage-assistant-demo.herokuapp.com/images/retired.jpeg', 'retirement')
+    )
   )
 }
 
 const healthBenefitIntent = (app) => {
   console.log('--> healthBenefit intent called')
-  app.ask('Ok, do you want me to find a doctor covered under your plan or would you like help with something else?')
+  // app.ask('Ok, do you want me to find a doctor covered under your plan or would you like help with something else?')
+  app.askWithList(app.buildRichResponse()
+    .addSimpleResponse('Ok, do you need help finding a doctor or something else?')
+    .addSuggestions(
+      ['Other']),
+    app.buildList('Medical Benefits')
+    .addItems(app.buildOptionItem('Doctor', ['doctor'])
+      .setTitle('Find a doctor')
+      .setDescription('Find specialists and primary care physicians covered under your plan')
+      // .setImage('https://samanage-assistant-demo.herokuapp.com/images/healthcare.jpg', 'health')
+    )
+    .addItems(app.buildOptionItem(SPECIALIST, ['chiropractors', 'chiropractor', 'specialist'])
+      .setTitle('Co-Pays')
+      .setDescription('View details on your co-pay amounts')
+      // .setImage('https://samanage-assistant-demo.herokuapp.com/images/vacation.jpeg', 'time off')
+    )
+    .addItems(app.buildOptionItem('Details', ['coverage', 'details'])
+      .setTitle('Details')
+      .setDescription('View a comprehensive description of your insurance policy')
+      // .setImage('https://samanage-assistant-demo.herokuapp.com/images/retired.jpeg', 'retirement')
+    )
+  )
 }
 
 const specialistHealthBenefitIntent = (app) => {
   console.log('--> specialistHealthBenefit intent called')
   // may have to use ssml to create delay event
-  app.ask('Yes, your plan covers chiropractors and other specialists with a $75 co-pay for office visits. ' +
-          'Additionally co-pays for generic prescriptions are $15.\nIs there anything else I can help you with?')
-  // app.ask('Is there anything else I can help you with?')
+  app.ask('<speak xml:lang="en-US">Yes, your plan covers chiropractors and other specialists with a $75 co-pay for office visits. ' +
+          'Additionally co-pays for generic prescriptions are $15.\n<break time="1s"/>Is there anything else I can help you with?</speak>')
 }
 
 const incidentIntent = (app) => {
   console.log('--> incident intent called')
-  app.ask('I\'m sorry to hear that, let me look into this problem and I will get back to you in a minute.',
+  app.ask('<speak xml:lang="en-US">I\'m sorry to hear that, <break time="500ms"/>let me look into this problem and I will get back to you in a minute.',
     ['I\'ve found that multiple users have reported a similar issue. Would you like me to submit a support ticket on your behalf?'])
 
   // app.ask('<speak xml:lang="en-US">I am sorry to hear that, let me look into this problem<break time="2000ms"/>' +
@@ -69,7 +89,7 @@ const incidentIntent = (app) => {
 
 const incidentSupportIntent = (app) => {
   console.log('--> incidentSupport intent called')
-  app.ask('<speak xml:lang="en-US"><break time="5s"/>' +
+  app.ask('<speak xml:lang="en-US"><break time="500ms"/>' +
     'I\'ve found that multiple users have reported a similar issue. Would you like me to submit a support ticket on your behalf?</speak>')
 }
 
@@ -82,13 +102,13 @@ const incidentSubmitIntent = (app) => {
 // Maybe make an intent with the trigger phrase "Talk to Sam"? (not sure if Google will like this...)
 const incidentUpdateIntent = (app) => {
   console.log('--> incidentUpdate intent called')
-  app.ask('Your ticket information has been sent to 919 586 1684')
-  // this is where we send the text
+  app.ask('<speak xml:lang="en-US">Your ticket information has been sent to <say-as interpret-as="telephone">+1 919-586-1684</say-as></speak>')
+  // this is where we send the text message
 }
 
 const incidentWelcomeIntent = (app) => {
   console.log('--> incidentWelcomeintent called')
-  app.ask('Nice to hear from you Devin, is your email issue resolved?')
+  app.ask('<speak xml:lang="en-US">Nice to hear from you Devin, <break time="500ms"/>is your email issue resolved?</speak>')
 }
 
 const incidentFollowUpIntent = (app) => {
